d27bd389c4986a5794e12f2c67d1a26e56819b1d
haydstew <haydstew@users.noreply.github.com>
2025-02-23 22:02:27 +0000

    deploy: 4cc739f0ccb4fd40dc2d9ea143f23f3eb82db3e9

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -4,34 +4,34 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 
-const toggleLink = document.getElementById("toggleForm");
+const toggleLink = document.querySelector("toggle-form-link");
 const formTitle = document.getElementById("formTitle");
 const actionButton = document.getElementById("actionButton");
 const toggleLinkText = document.getElementById("toggleLink");
 
 let isRegistering = false;
 
-toggleLink.addEventListener("click", (event) => {
+function toggleForm(event) {
   event.preventDefault();
 
   isRegistering = !isRegistering;
 
   if (isRegistering) {
-    // Switch to Register form
     formTitle.textContent = "Register";
     actionButton.textContent = "Register";
     toggleLinkText.innerHTML =
-      'Already have an account? <a href="#" id="toggleForm">Sign In here</a>';
+      'Already have an account? <a href="#" class="toggle-form-link">Sign in here</a>';
   } else {
-    // Switch to Sign In form
     formTitle.textContent = "Sign In";
     actionButton.textContent = "Sign In";
     toggleLinkText.innerHTML =
-      'Don\'t have an account? <a href="#" id="toggleForm">Register here</a>';
+      'Don\'t have an account? <a href="#" class="toggle-form-link">Register here</a>';
   }
-});
 
-// Handle form submission for Sign In and Register
+  const newToggleLink = document.querySelector(".toggle-form-link");
+  newToggleLink.addEventListener("click", toggleForm);
+}
+
 async function registerBiometric() {
   const userEmail = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -46,7 +46,6 @@ async function registerBiometric() {
     alert(
       "You have been registered successfully! You can now sign into your account."
     );
-    // Handle additional biometric registration here if needed
   } catch (error) {
     console.error("Registration failed:", error);
     alert("An error occurred during registration. Please ry again.");
@@ -71,7 +70,6 @@ async function authenticateBiometric() {
   }
 }
 
-// Add event listeners based on form mode (sign-in or register)
 actionButton.addEventListener("click", () => {
   if (isRegistering) {
     registerBiometric();
@@ -79,3 +77,5 @@ actionButton.addEventListener("click", () => {
     authenticateBiometric();
   }
 });
+
+toggleLink.addEventListener("click", toggleForm);
