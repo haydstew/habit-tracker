647e02e044ecb0562ecbfa55d876b448a2354b06
haydstew <haydstew@users.noreply.github.com>
2025-02-23 22:30:28 +0000

    deploy: 06630c742669f5ac4bee7ab3589f965d227359d1

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -32,6 +32,17 @@ function toggleForm(event) {
   newToggleLink.addEventListener("click", toggleForm);
 }
 
+function base64urlToUint8Array(base64url) {
+  const padding = "=".repeat((4 - (base64url.length % 4)) % 4);
+  const base64 = (base64url + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 async function registerBiometric() {
   // Get values inside the function, when the button is clicked
   const userEmail = document.getElementById("email").value.trim();
